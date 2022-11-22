7d96b410cab2041abaeeace1314e05266ef53b64
Joseph Howe <joseph.howe54@gmail.com>
2022-11-21 22:38:14 -0500

    bio padding

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       </header>
       <main>
         <img />
-        <p id="bio" className="bg-black text-white p-20">
+        <p id="bio" className="bg-black text-white lg:p-20 md:p-10 p-4">
         Secret Formula Band is a southern rock group out of Charlotte, North Carolina, that has been together since February 2022. The group brings a rugged, soulful noise to the stage that sounds as good as Miller Lite tastes. Drawing inspiration from some of their favorite blues, rock, and country artists – BB King, Pink Floyd, Tyler Childers, Lynyrd Skynyrd, and Grateful Dead - the group brings dynamic, improvisational music with a distinctly southern flair. 
 
 Mac McArtor [vocals, keys], Andrew Miramon [guitar, vocals], Alex Weir [guitar, mandolin, vocals], David Kirkpatrick [bass], and Joseph Howe [drums] make up the 5-piece machine that is Secret Formula Band as we know it today, with original members David Gardener, Ashby Wickham, and Matt Ziegelstein frequently coming home to jam with the Formulads. 

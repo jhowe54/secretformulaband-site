27889b6378562bbecba9e71cdae1257614acbd93
jhowe54 <joseph.howe54@gmail.com>
2022-11-23 12:45:04 -0500

    updated svg and media

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { BiMenu } from "react-icons/bi";
-
+import {ReactComponent as SnailLogo} from './snailfinal.svg';
+import group from "./group.JPG"
 function App() {
   return (
     <div className="App flex flex-col">
       <header
         id="header"
-        className="flex flex-row p-2 justify-end text-white text-6xl  h-[40vh] bg-black   "
+        className="flex flex-col justify-center content-center text-white text-4xl bg-black "
       >
         <BiMenu
-          size={60}
-          className="self-start p-2 cursor-pointer hover:text-slate-200"
+          size={70}
+          className="absolute md:top-0 md:right-0 md:visible invisible cursor-pointer hover:text-slate-200 p-2 mx-auto"
         />
+       <SnailLogo fill="#FFFFFF" stroke="#FFFFFF" color="#FFFFFF" className=" mx-auto bg w-full"/>
+        
+        
       </header>
-      <main className="bg-black text-white">
-        <img />
+      <main className="flex flex-col items-center bg-black text-white">
+        <img src={group} className="lg:w-2/3 w-full xl:p-0  rounded-md my-8 box-content" />
         <p id="bio-1" className=" lg:px-20 md:px-10 px-4 py-4">
           Secret Formula Band is a southern rock group out of Charlotte, North
           Carolina, that has been together since February 2022. The group brings

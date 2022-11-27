686f2712e0a90d78e6451e6fb3513c4884df50e4
jhowe54 <joseph.howe54@gmail.com>
2022-11-27 18:02:11 -0500

    added insta feed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BiMenu } from "react-icons/bi";
 import { FaInstagram } from "react-icons/fa";
 import { ReactComponent as SnailLogo } from "./snailfinal.svg";
+import { ReactDOM } from "react";
+
 import group from "./group.JPG";
+import InstaFeed from "./InstaFeed";
 function App() {
   return (
     <div className="App flex flex-col">
@@ -18,6 +21,7 @@ function App() {
           stroke="#FFFFFF"
           color="#FFFFFF"
           className=" mx-auto bg w-full"
+          
         />
       </header>
       <main className="flex flex-col items-center bg-black text-white">
@@ -25,7 +29,7 @@ function App() {
           src={group}
           className="lg:w-2/3 w-full xl:p-0  rounded-md my-8 box-content"
         />
-        <p id="bio-1" className=" lg:px-20 md:px-10 px-4 py-4">
+        <p id="bio-1" className=" lg:px-20 md:px-10 px-6 py-4">
           Secret Formula Band is a southern rock group out of Charlotte, North
           Carolina, that has been together since February 2022. The group brings
           a rugged, soulful noise to the stage that sounds as good as Miller
@@ -34,7 +38,7 @@ function App() {
           Lynyrd Skynyrd, and Grateful Dead - the group brings dynamic,
           improvisational music with a distinctly southern flair.
         </p>
-        <p id="bio-2" className="lg:px-20 md:px-10 px-4 py-4">
+        <p id="bio-2" className="lg:px-20 md:px-10 px-6 py-4">
           Mac McArtor [vocals, keys], Andrew Miramon [guitar, vocals], Alex Weir
           [guitar, mandolin, vocals], David Kirkpatrick [bass], and Joseph Howe
           [drums] make up the 5-piece machine that is Secret Formula Band as we
@@ -42,8 +46,8 @@ function App() {
           and Matt Ziegelstein frequently coming home to jam with the Formulads.
         </p>
         <section className="flex flex-col justify-center content-center items-center w-full text-white p-4 text-center">
-          <h2 className="text-5xl mb-2">Media</h2>
-          {/*Instagram feed */}
+          <h2 className="text-5xl mb-4">Media</h2>
+            <InstaFeed />
           <iframe
             className="my-4"
             id="video"

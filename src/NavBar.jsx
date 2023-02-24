d69d320d61195e9d54733789ce7d7d51174e6d3c
jhowe54 <joseph.howe54@gmail.com>
2023-02-24 18:34:56 -0500

    quickfix

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,18 +6,18 @@ function NavBar() {
     <nav id="navbar" className="w-full mx-auto">
       <div className="w-full text-lg p-6 flex flex-row md:flex-nowrap flex-wrap md:justify-end items-center justify-center">
         <div className="nav-right flex flex-end ">
-          <Link to={"/"} className="text-white md:p-4 p-2">
+          <Link to={"/"} href="#home" className="text-white md:p-4 p-2">
             Home
           </Link>
-          <Link className="text-white md:p-4 p-2 " href="#media">
+          <a className="text-white md:p-4 p-2" href="/#media">
             Media
-          </Link>
-          <Link className="text-white md:p-4 p-2 self end" href="#shows">
+          </a>
+          <a className="text-white md:p-4 p-2" href="/#shows">
             Shows
-          </Link>
+          </a>
           <Link
             to={"/bookings"}
-            className="text-white md:p-4 p-2 flex-1 self-end"
+            className="text-white md:p-4 p-2"
           >
             Book a Show
           </Link>

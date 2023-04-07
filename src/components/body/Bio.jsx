71c99082b87fbe5b4ad55676c975f1561bb9051e
jhowe54 <joseph.howe54@gmail.com>
2023-04-07 13:20:41 -0400

    media updates

--- src/components/body/Bio.jsx
+++ src/components/body/Bio.jsx
@@ -1,10 +1,11 @@
 import group from "../../media/group.JPG";
+import outsidepic2 from "../../media/IMG_0590.png";
 function Bio() {
   return (
     <>
       <img
-        src={group}
-        className="lg:w-2/3 w-full xl:p-0  rounded-md my-8 box-content"
+        src={outsidepic2}
+        className="lg:w-1/2 w-full xl:p-0  rounded-md my-8 box-content grayscale contrast-[1.4]"
         alt="Band Pic"
       />
       <p id="bio-1" className=" lg:px-60 md:px-10 px-4 py-4 lg:text-xl">
@@ -16,7 +17,7 @@ function Bio() {
         together since February 2022. The group brings a rugged, soulful noise
         to the stage that sounds as good as Miller Lite tastes. Drawing
         inspiration from some of their favorite blues, rock, and country artists
-        – BB King, Pink Floyd, Tyler Childers, Lynyrd Skynyrd, and Grateful Dead
+        - BB King, Pink Floyd, Tyler Childers, Lynyrd Skynyrd, and Grateful Dead
         - the group brings dynamic, improvisational music with a distinctly
         southern flair.
       </p>
--- src/components/body/Social.jsx
+++ src/components/body/Social.jsx
@@ -6,9 +6,9 @@ function Social() {
   return (
     <div className="w-full">
       <h2 id="social" className="text-5xl my-2">
-        Follow us on Social Media
+        Social Media
       </h2>
-      <div className="flex md:flex-row md:wrap-none flex-col justify-center gap-14 md:p-12 p-6">
+      <div className="flex md:flex-row md:wrap-none flex-col justify-center md:gap-12 gap-6 md:p-12 p-6">
         <div className=" flex flex-col items-center">
           <a id="insta" href="https://www.instagram.com/secretformulaband/" target="_blank" rel="noreferrer">
             <FaInstagram id="insta-icon" size={70} />
@@ -32,6 +32,7 @@ function Social() {
         }
 
       </div>
+      <a href="mailto:secretformulaband@gmail.com">email: <span className="text-blue-300 text-sm">secretformulaband@gmail.com</span></a>
     </div>
   );
 }
